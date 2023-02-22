4fe8a6cd364f90bb0a5574467364a47a7f91bbeb
jenkinstesting0123 <124963018+jenkinstesting0123@users.noreply.github.com>
2023-02-22 15:33:11 +0530

    Update wpinstall-sync.py

--- wpinstall-sync.py
+++ wpinstall-sync.py
@@ -58,18 +58,18 @@ def main(argv):
     host_name='' 
     database_name=''
     table_name=''
-    file_name = ''
+    file_name =''
     
     if validateParams():
         try:
           opts, args = getopt.getopt(sys.argv[1:], 'dluphdtf:', ['dumpdata', 'loaddata','user_name','password','host_name','database_name','table_name','file_name'])
-	  print(opts)
         except getopt.GetoptError:
             print('error in arguments')
         for opt, arg in opts:
             #MessageTemplate.showMessageInConsole('opt: {0} - arg: {1}'.format(opt, arg))
             if opt in ('-u', '--user_name'):
-                user_name=arg    
+                user_name=arg
+		print(user_name)
             elif opt in ('-p', '--password'):
                 password = arg
             elif opt in ('-h', '--host_name'):
