83fc378a85571f7a3e00dfb211c1b17f4fef5ddf
jenkinstesting0123 <124963018+jenkinstesting0123@users.noreply.github.com>
2023-02-22 15:10:30 +0530

    Update wpinstall-sync.py

--- wpinstall-sync.py
+++ wpinstall-sync.py
@@ -13,8 +13,8 @@ def datadump(user_name, password, host_name, database_name, table_name, file_nam
         MessageTemplate.showMessageInConsole('Downloading from SIDIOUS')
         #cmd = mysqldump --login-path=dev-sidious --force production full_usbus > full_usbus.sql
         cmd = "mysqldump -u "+user_name+ " –p" +password+ " -h" +host_name+ " " +database_name+ " "+table_name+ " >" +file_name
-	#print(cmd.decode('utf-8'))
-        os.system(cmd)
+	print(cmd.decode('ascii'))
+        #os.system(cmd)
 
         #MessageTemplate.NotifyMessage('Succeed', 'From Sidious', 'Data downloaded successfully!', Constants.MAIL_RECIPIENTS_COMPLETE)
     except:
